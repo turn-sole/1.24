9f56082b000577e2a3c5a665b8137658319978e7
turn-sole <113235914+turn-sole@users.noreply.github.com>
2023-01-24 21:45:37 +0800

    Add files via upload

---
+++ fdak.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+//int main()
+//{
+//	unsigned int i;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//		Sleep(1000);
+//	}
+//	return 0;
+//}
+int main()
+{
+	int n = 9;
+	float* pFloat = (float*)&n;
+	printf("n的值为：%d\n", n);
+	printf("*pFloat的值为：%f\n", *pFloat);
+	*pFloat = 9.0;
+	printf("num的值为：%d\n", n);
+	printf("*pFloat的值为：%f\n", *pFloat);
+	return 0;
+}
+//int main()
+//{
+//	float f = 5.5f;
+//	return 0;
+//}
